a6a87e50dc4e17a976176db58e2c74e563037caf
onats1 <donatemowo@yahoo.com>
2021-04-21 23:49:59 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        jcenter() // Warning: this repository is going to shut down soon
+    }
+}
+rootProject.name = "RickFandom"
+include(":app")
+include(":features:characters")
+include(":features:episodes")
+include(":features:location")
+include(":core-android")
