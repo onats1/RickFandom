b60ee5c9d4581f363ba8f2691a825e6a1da3a166
damilareonatemowo <onatsofficial@gmail.com>
2021-07-11 08:37:53 +0100

    Finished unit testing characters data source

--- data/characters-remote/src/test/java/com/onats/characters_remote/CharactersRemoteDataSourceTest.kt
+++ data/characters-remote/src/test/java/com/onats/characters_remote/CharactersRemoteDataSourceTest.kt
@@ -11,15 +11,19 @@ import com.onats.core_android_character.CharactersResponse
 import com.onats.core_android_character.LocationDto
 import com.onats.core_android_character.OriginDto
 import com.onats.core_android_character.data.CharactersApiService
+import com.onats.core_character.models.Character
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.`when`
 import retrofit2.Response
+import java.io.IOException
 
 @ExperimentalStdlibApi
-class CharactersRemoteDataSourceTest {
+internal class CharactersRemoteDataSourceTest {
 
     private val mockApiService = mock(CharactersApiService::class.java)
     private val mockCharacterMapper = CharacterMapperImpl(
@@ -32,7 +36,7 @@ class CharactersRemoteDataSourceTest {
     @Test
     fun `test that success response returns list of characters`() = runBlocking {
         `when`(mockApiService.getAllCharacters()).thenAnswer {
-            return@thenAnswer Response.success<CharactersResponse>(
+            Response.success<CharactersResponse>(
                 characterNetworkResponse
             )
         }
@@ -46,7 +50,42 @@ class CharactersRemoteDataSourceTest {
             assertThat(result.error).isEqualTo(ApplicationError.NoError)
             assertThat(result.data).isEqualTo(characterList)
         }
+    }
 
+    @Test
+    fun `test that error response returns application network error`() = runBlocking {
+        `when`(mockApiService.getAllCharacters()).thenAnswer {
+             Response.error<List<Character>>(
+                404, "Page not found"
+                    .toResponseBody("application/json".toMediaTypeOrNull())
+            )
+        }
+        val dataSource = CharactersRemoteDataSourceImpl(
+            charactersApiService = mockApiService,
+            charactersMapper = mockCharacterMapper
+        )
+
+        dataSource.getAllCharacters().collect { characterResult ->
+            assertThat(characterResult.error).isInstanceOf(ApplicationError.NetworkError::class.java)
+            assertThat(characterResult.data).isNull()
+        }
+    }
+
+    @Test
+    fun `test that network exception is caught as network exception error`() = runBlocking {
+        `when`(mockApiService.getAllCharacters()).thenAnswer {
+            throw IOException()
+        }
+        val dataSource = CharactersRemoteDataSourceImpl(
+            charactersApiService = mockApiService,
+            charactersMapper = mockCharacterMapper
+        )
+
+        dataSource.getAllCharacters().collect { characterResult ->
+            assertThat(characterResult.error).isInstanceOf(ApplicationError.NetworkError::class.java)
+            assertThat(characterResult.data).isNull()
+            assertThat((characterResult.error as ApplicationError.NetworkError).errorException).isNotNull()
+        }
     }
 }
 
