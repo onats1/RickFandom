03b171ffc9e491821510dcaced98b73e4ae19a5b
damilareonatemowo <onatsofficial@gmail.com>
2021-07-12 10:51:57 +0100

    UI bug fixes.

--- ui-components/core-ui/src/main/java/com/onats/common_ui/components/TopAppBar.kt
+++ ui-components/core-ui/src/main/java/com/onats/common_ui/components/TopAppBar.kt
@@ -3,18 +3,20 @@ package com.onats.common_ui.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.onats.common_ui.theme.RickFandomTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.draw.clip
 
 @Composable
 fun AppBarInfo(
@@ -26,7 +28,7 @@ fun AppBarInfo(
             .height(110.dp)
             .fillMaxWidth()
     ) {
-        var text by remember { mutableStateOf("")} // Will be updated accordingly
+        var text by remember { mutableStateOf("") } // Will be updated accordingly
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
@@ -37,10 +39,23 @@ fun AppBarInfo(
                 value = text,
                 onValueChange = { text = it },
                 modifier = Modifier
-                    .background(color = Color.White)
                     .fillMaxWidth(.9f)
                     .wrapContentHeight()
-                    .clip(MaterialTheme.shapes.medium),
+                ,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = "Search"
+                    )
+                },
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    leadingIconColor = MaterialTheme.colors.secondary,
+                    focusedBorderColor = MaterialTheme.colors.primaryVariant,
+                    unfocusedBorderColor = MaterialTheme.colors.primaryVariant,
+                    textColor = Color.Black,
+                    cursorColor = Color.Black
+                ),
+                shape = MaterialTheme.shapes.medium
             )
             Spacer(modifier = Modifier.height(12.dp))
         }
@@ -51,7 +66,7 @@ fun AppBarInfo(
 @Composable
 fun PreviewAppBar() {
     RickFandomTheme {
-        AppBarInfo(title = "Characters")
+        AppBarInfo(title = "Title")
     }
 }
 
