09286ec2703391023a4ace53cae05c8d268e2e4e
damilareonatemowo <onatsofficial@gmail.com>
2022-03-08 07:54:01 +0100

    Character summary card clickable test

--- ui-components/characters-ui-components/src/androidTest/java/com/onats/characters_ui_components/uitests/CharacterSummaryCardTest.kt
+++ ui-components/characters-ui-components/src/androidTest/java/com/onats/characters_ui_components/uitests/CharacterSummaryCardTest.kt
@@ -2,7 +2,9 @@ package com.onats.characters_ui_components.uitests
 
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import com.onats.characters_ui_components.presentation.components.CharacterSummaryCard
 import org.junit.Rule
 import org.junit.Test
@@ -28,5 +30,19 @@ class CharacterSummaryCardTest {
         nameVariable.assertExists()
         specieVariable.assertExists()
         episodesVariable.assertExists()
+    }
+
+    @Test
+    fun testThatCharacterCardIsClickable() {
+        var clicked = false
+        composeTestRule.setContent {
+            CharacterSummaryCard(
+                character = testCharacterSummary,
+                onCharacterClick = { clicked = true }
+            )
+        }
+        val characterCard = composeTestRule.onNodeWithTag("characterCard")
+        characterCard.performClick()
+        assert(clicked)
     }
 }
--- ui-components/characters-ui-components/src/main/java/com/onats/characters_ui_components/presentation/components/CharacterThumbnail.kt
+++ ui-components/characters-ui-components/src/main/java/com/onats/characters_ui_components/presentation/components/CharacterThumbnail.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +32,8 @@ fun CharacterSummaryCard(
         modifier = Modifier
             .fillMaxWidth()
             .padding(end = 8.dp, bottom = 8.dp)
-            .wrapContentHeight(),
+            .wrapContentHeight()
+            .testTag("characterCard"),
         shape = MaterialTheme.shapes.medium,
         backgroundColor = Color.LightGray
     ) {
